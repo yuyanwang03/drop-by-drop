bb0a02f5b96d6882af3aeefffbe1460a1fc4a5bc
martioms01 <marti.oms01@estudiant.upf.edu>
2024-11-28 20:10:54 +0100

    Final Display a faltar d'enllaçcar amb model

--- pipelines/fetch_data_for_prediction.py
+++ pipelines/fetch_data_for_prediction.py
@@ -13,8 +13,6 @@ def prepare_dataset_for_prediction(pandas_dataset, prediction_final_day, pernoct
     Returns:
         pd.DataFrame: Modified dataset with additional rows and filled weather columns.
     """
-    # Convert prediction_final_day to datetime
-    prediction_final_day = datetime.strptime(prediction_final_day, "%Y-%m-%d")
     
     # Get the last date in the dataset
     last_date = pd.to_datetime(pandas_dataset['Date'].max())
@@ -44,7 +42,6 @@ def prepare_dataset_for_prediction(pandas_dataset, prediction_final_day, pernoct
     
     # Ensure the Date column in the original dataset is datetime format is YYY-MM-DD
     pandas_dataset['Date'] = pd.to_datetime(pandas_dataset['Date'], format='%Y-%m-%d')
-
     
     # Merge new rows with the original dataset on Census Section, District, and Previous Year Date
     merged_df = pd.merge(
@@ -92,21 +89,3 @@ def prepare_dataset_for_prediction(pandas_dataset, prediction_final_day, pernoct
     updated_dataset = pd.concat([pandas_dataset, new_rows_df], ignore_index=True)
 
     return updated_dataset
-
-
-
-# Load the dataset
-dataset = pd.read_csv('../data/local_data/merged_cleaned_data_NEW.csv')
-
-
-# Prepare the dataset for prediction (IVAN ESTO SUSTITUYELO POR LOS DATOS DE STREAMLIT)
-prediction_final_day = '2024-02-14'
-pernoctation_mean = 2.5
-tourist_distribution = pd.read_csv('martigay.csv')
-# Drop all rows except Tourists
-tourist_distribution = tourist_distribution.drop(columns=['Id'])
-
-updated_dataset = prepare_dataset_for_prediction(dataset, prediction_final_day, pernoctation_mean, tourist_distribution)
-
-# Save the updated dataset
-updated_dataset.to_csv('../data/local_data/updated_dataset_for_prediction.csv', index=False)
